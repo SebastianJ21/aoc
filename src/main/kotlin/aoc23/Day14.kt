91229209115c49814335a6186f07b1e7a8876d42
SJakabcin <100082184+sebastianj21@users.noreply.github.com>
2024-11-05 14:12:59 +0100

    aoc23/Day14: Improve solution
    Also implement the AOCSolution interface

--- src/main/kotlin/aoc23/Day14.kt
+++ src/main/kotlin/aoc23/Day14.kt
@@ -1,71 +1,78 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package aoc23
 
+import AOCAnswer
+import AOCSolution
 import AOCYear
-import convertInputToCharArrayMatrix
+import convertInputToCharMatrix
 import readInput
 import transposed
 
-class Day14 {
+class Day14 : AOCSolution {
     enum class Direction { N, W, S, E }
 
-    fun solve() {
-        val rawInput = readInput("day17.txt", AOCYear.TwentyThree)
-        val matrix = convertInputToCharArrayMatrix(rawInput)
-
-        fun spinMatrix(matrix: Array<Array<Char>>) =
-            Direction.entries.fold(matrix) { acc, direction -> getMatrixTilted(acc, direction) }
-
-        fun performNMatrixSpins(matrix: Array<Array<Char>>, n: Int): Array<Array<Char>> {
-            val cache = mutableMapOf<Int, Pair<Array<Array<Char>>, Int>>()
-            return (1..n).fold(matrix) { currentMatrix, roundIndex ->
-                val hash = currentMatrix.contentDeepHashCode()
-
-                if (hash in cache) {
-                    val cycleStartIndex = cache[hash]!!.second
-                    val cycleSize = roundIndex - cycleStartIndex
-                    val answerIndex = (n - cycleStartIndex) % cycleSize + cycleStartIndex
-
-                    return cache.values.firstNotNullOf { (cachedMatrix, index) ->
-                        cachedMatrix.takeIf { index == answerIndex }
-                    }
-                }
-
-                val nextMatrix = spinMatrix(currentMatrix)
-                cache[hash] = nextMatrix to roundIndex
-
-                nextMatrix
-            }
-        }
-
-        fun calculateMatrixScore(matrix: Array<Array<Char>>) = matrix.indices.sumOf { rowI ->
-            matrix[rowI].count { it == 'O' } * (matrix.size - rowI)
-        }
+    override fun solve(): AOCAnswer {
+        val rawInput = readInput("day14.txt", AOCYear.TwentyThree)
+        val matrix = convertInputToCharMatrix(rawInput)
 
         val partOne = calculateMatrixScore(getMatrixTilted(matrix, Direction.N))
         val partTwo = calculateMatrixScore(performNMatrixSpins(matrix, 1000000000))
 
-        println("Part one: $partOne")
-        println("Part two: $partTwo")
+        return AOCAnswer(partOne, partTwo)
     }
 
-    fun getMatrixTilted(matrix: Array<Array<Char>>, direction: Direction): Array<Array<Char>> {
-        val transpositionSelector = { arr: Array<Array<Char>> ->
-            if (direction in listOf(Direction.N, Direction.S)) arr.transposed() else arr
+    fun spinMatrix(matrix: List<List<Char>>) =
+        Direction.entries.fold(matrix) { acc, direction -> getMatrixTilted(acc, direction) }
+
+    fun performNMatrixSpins(matrix: List<List<Char>>, n: Int): List<List<Char>> {
+        val cache = mutableMapOf<List<List<Char>>, Pair<List<List<Char>>, Int>>()
+
+        return (1..n).fold(matrix) { currentMatrix, roundIndex ->
+            val cycleStartIndex = cache[currentMatrix]?.second
+
+            if (cycleStartIndex != null) {
+                val cycleSize = roundIndex - cycleStartIndex
+                val answerIndex = (n - cycleStartIndex) % cycleSize + cycleStartIndex
+
+                return cache.values.firstNotNullOf { (cachedMatrix, index) ->
+                    cachedMatrix.takeIf { index == answerIndex }
+                }
+            }
+
+            val nextMatrix = spinMatrix(currentMatrix)
+
+            cache[currentMatrix] = nextMatrix to roundIndex
+
+            nextMatrix
+        }
+    }
+
+    fun calculateMatrixScore(matrix: List<List<Char>>) = matrix.indices.sumOf { rowI ->
+        matrix[rowI].count { it == 'O' } * (matrix.size - rowI)
+    }
+
+    fun getMatrixTilted(matrix: List<List<Char>>, direction: Direction): List<List<Char>> {
+        val transpositionSelector = { list: List<List<Char>> ->
+            if (direction in listOf(Direction.N, Direction.S)) list.transposed() else list
         }
         val rowOrderSelector = { space: String, rocks: String ->
             if (direction in listOf(Direction.N, Direction.W)) rocks + space else space + rocks
         }
 
-        return transpositionSelector(matrix)
-            .map { row ->
-                row.joinToString("")
-                    .split("#")
-                    .joinToString("#") { rocksAndSpace ->
-                        rocksAndSpace.partition { it == '.' }.let { (space, rocks) -> rowOrderSelector(space, rocks) }
-                    }
-                    .toCharArray()
-                    .toTypedArray()
-            }.toTypedArray()
-            .run(transpositionSelector)
+        val transposed = transpositionSelector(matrix)
+
+        val transformed = transposed.map { row ->
+            row.joinToString("")
+                .split("#")
+                .joinToString("#") { rocksAndSpace ->
+                    val (space, rocks) = rocksAndSpace.partition { it == '.' }
+
+                    rowOrderSelector(space, rocks)
+                }.toList()
+        }
+
+        val result = transpositionSelector(transformed)
+        return result
     }
 }
