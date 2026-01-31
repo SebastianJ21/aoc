e6aa41bf8a44dae08c54a444129c1fca7b540417
sebastian.jakabcin <sebastian.jakabcin@cleevio.com>
2026-01-31 17:27:50 +0100

    aoc25/Day4: Use non-generic defined Position (for performance)

--- src/main/kotlin/aoc25/Day4.kt
+++ src/main/kotlin/aoc25/Day4.kt
@@ -2,35 +2,39 @@ package aoc25
 
 import AOCAnswer
 import AOCSolution
-import applyDirection
 import countUntil
-import plus
 import positionsOf
 import readInput
 
 class Day4 : AOCSolution {
 
-    val up = -1 to 0
-    val down = 1 to 0
-    val left = 0 to -1
-    val right = 0 to 1
+    private val up = Position(-1, 0)
+    private val down = Position(1, 0)
+    private val left = Position(0, -1)
+    private val right = Position(0, 1)
+
+    private data class Position(val first: Int, val second: Int)
+    private operator fun Position.plus(other: Position): Position = Position(first + other.first, second + other.second)
 
     private val neighborhood = listOf(up + left, up, up + right, right, down + right, down, down + left, left)
 
     override fun solve(): AOCAnswer {
         val rawInput = readInput("day4.txt", AOCYear.TwentyFive)
 
-        val initialRollPositions = rawInput.map { line -> line.toList() }.positionsOf { it == '@' }.toSet()
+        val initialRollPositions = rawInput.map { line -> line.toList() }
+            .positionsOf { it == '@' }
+            .map { Position(it.first, it.second) }
+            .toSet()
 
         val rollsCountSequence = generateSequence(initialRollPositions) { rollPositions ->
             val nextPositions = rollPositions.filter { position ->
-                neighborhood.countUntil(4) { direction -> position.applyDirection(direction) in rollPositions } >= 4
+                neighborhood.countUntil(4) { direction -> position + direction in rollPositions } >= 4
             }
 
             nextPositions.toSet()
         }.map { it.size }
 
-        val partOne = rollsCountSequence.zipWithNext { initial, second -> initial - second }.first()
+        val partOne = rollsCountSequence.take(2).toList().let { (initial, second) -> initial - second }
 
         // Take while some rolls are removed
         val finalCount = rollsCountSequence.zipWithNext().takeWhile { (a, b) -> a > b }.last().second
